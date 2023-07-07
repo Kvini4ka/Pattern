0d0e3bbea4b7af70d1b970d6b96882d6b2adf162
kvini4ka <kvini4ka@mail.ru>
2023-07-07 14:35:36 +0300

    recover packadge 2

--- src/test/java/test/DeliveryCardTest.java
+++ src/test/java/test/DeliveryCardTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import Data.DataGenerator;
+import data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
